028bbdfe7cab7734ec9c29ebb68cd89afc978f56
Yuri Stanzani <ystanzani@gmail.com>
2025-06-01 15:59:27 -0300

    chore: set up initial Redux and Redux Toolkit configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import GlobalCss from './styles'
+
 import Header from './components/Header'
+import GlobalCss from './styles'
 
 import PagesRoutes from './routes'
 import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="centralizer">
-        <Header />
-      </div>
-      <PagesRoutes />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="centralizer">
+          <Header />
+        </div>
+        <PagesRoutes />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
